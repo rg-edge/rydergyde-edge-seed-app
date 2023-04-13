4e52aeaebd734fa05ec3656213896401fb2ad9fb
matiasmoron <matiasmoron008@gmail.com>
2023-04-13 15:11:21 -0300

    refactor: override package.json

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,3 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document from '@ryder/ui/pages/Document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-}
+export default Document;
